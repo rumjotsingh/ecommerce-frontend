30106e23ed5ccf12566f2c542aa50b0124defa64
rumjotsingh <155811126+rumjotsingh@users.noreply.github.com>
2024-11-02 10:13:33 +0530

    Update Login.js

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("https://ecommerce-backend-s84l.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
