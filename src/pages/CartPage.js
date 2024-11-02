7574fd4e17168070cc52bd88834191655e3bd2e7
rumjotsingh <155811126+rumjotsingh@users.noreply.github.com>
2024-11-02 10:16:32 +0530

    Update CartPage.js

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -46,7 +46,7 @@ const CartPage = () => {
   //get payment gateway token
   const getToken = async () => {
     try {
-      const { data } = await axios.get("/api/v1/product/braintree/token");
+      const { data } = await axios.get("https://ecommerce-backend-s84l.onrender.com/api/v1/product/braintree/token");
       setClientToken(data?.clientToken);
     } catch (error) {
       console.log(error);
@@ -61,7 +61,7 @@ const CartPage = () => {
     try {
       setLoading(true);
       const { nonce } = await instance.requestPaymentMethod();
-      const { data } = await axios.post("/api/v1/product/braintree/payment", {
+      const { data } = await axios.post("https://ecommerce-backend-s84l.onrender.com/api/v1/product/braintree/payment", {
         nonce,
         cart,
       });
@@ -98,7 +98,7 @@ const CartPage = () => {
               <div className="row mb-2 p-3 card flex-row">
                 <div className="col-md-4">
                   <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
+                    src={`https://ecommerce-backend-s84l.onrender.com/api/v1/product/product-photo/${p._id}`}
                     className="card-img-top"
                     alt={p.name}
                     width="100px"
